48e2dce725e1d523c5352b1f7ccd9fbdc4b60fc6
facufachin <facusangui1976@gmail.com>
2023-11-04 23:41:08 +0000

    v1 release

---
+++ index.js
@@ -0,0 +1,9 @@
+function copyToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+  alert("Contraseña copiada");
+}
